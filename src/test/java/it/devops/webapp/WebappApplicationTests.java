19be335b72a9be82b4922981531ab800b299d065
dargentieri.devops <dev@domenico-argentieri.it>
2025-03-01 19:50:03 +0100

    init

---
+++ src/test/java/it/devops/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devops.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
